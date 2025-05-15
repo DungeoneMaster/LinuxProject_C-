0cf09531f0f07effd481285d2bacbc9558e40d58
DungeoneMaster <ivan.m3prosto@mail.ru>
2025-05-15 11:53:28 +0700

    pushBoss23

--- main.cpp
+++ main.cpp
@@ -1,46 +1,33 @@
 #include<iostream>
 #include<vector>
 #include<cmath>
-void Euler(double x0, double y0, double Vx0, double Vy0, double h, int& n, std::vector<double>& Netx, std::vector<double>& Nety) {
+
+using namespace std;
+
+void Euler(double x0, double y0, double Vx0, double Vy0, double h, vector<double>& Netx, vector<double>& Nety) {
     double x = x0;
     double y = y0;
     double Vx = Vx0;
     double Vy = Vy0;
-    int N = -1;
     do {
-        ++N;
         x0 = x;
         y0 = y;
         Vx0 = Vx;
         Vy0 = Vy;
+
         x = x0 + (h * Vx0);
         y = y0 + (h * Vy0);
+
         Netx.push_back(x);
         Nety.push_back(y);
+
         Vx = Vx0 + (h  * x0);
         Vy = Vy0 + (h * y0);
-        //std::cout << fabs(x * y - 0.07) << '/n';
-    } while ((x < 1) && (y < 1));
-    n = N - 1;
-    //return x0 * y0;
-}
 
-/*  do {
-    ++N;
-    x0 = x;
-    y0 = y;
-    Vx0 = Vx;
-    Vy0 = Vy;
-    x = x0 + (h * Vx0);
-    y = y0 + (h * Vy0);
-    Vx = Vx0 + (h * x0);
-    Vy = Vy0 + (h * y0);
-    std::cout << fabs(x * y - 0.07) << '/n';
-  } while ((x < 1) && (y < 1) && N < 20);
-  n = N - 1;
-  return x0 * y0;
+
+    } while ((x < 1) && (y < 1));
+
 }
-*/
 
 int main() {
     double x0 = 0.01 * 7;
@@ -51,17 +38,17 @@ int main() {
     double h = 0.1;
     double H = h / 2;
 
-    int k = 0,n = 0,m = 0;
+    int k = 0;
 
-    std::vector<double> Net1x,Net1y,Net2x,Net2y,resx,resy;
+    vector<double> Net1x,Net1y,Net2x,Net2y,resx,resy;
     Net1x.push_back(x0);
 
     Net1y.push_back(y0);
     Net2x.push_back(x0);
     Net2y.push_back(y0);
 
-    Euler(x0, y0, Vx0, Vy0, h, n, Net1x, Net1y);
-    Euler(x0, y0, Vx0, Vy0, H, m, Net2x, Net2y);
+    Euler(x0, y0, Vx0, Vy0, h, Net1x, Net1y);
+    Euler(x0, y0, Vx0, Vy0, H, Net2x, Net2y);
 
 
     for (int i = 0; i < Net1x.size()-1; ++i) {
@@ -70,7 +57,7 @@ int main() {
         ++k;
     }
     for (int i = 0; i < resx.size(); ++i)
-        std::cout << fabs(resx[i] * resy[i] - Ca);
+        cout << fabs(resx[i] * resy[i] - Ca) << "\n";
 
 
 }
